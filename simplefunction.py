325ddb112e512854b08b34bdcbcceec4bb3465db
lodyne <lodgmtui@gmail.com>
2021-10-12 15:31:32 +0300

    change name

--- simplefunction.py
+++ simplefunction.py
@@ -1,6 +1,6 @@
 def ManUnited(players,coach,captain):
     players = ['Pogba','Jesse','Rashford']
-    coach = 'Ole Gunnar'
+    coach = 'Ole Solskjaer'
     captain = 'Maguire'
     print(players,coach,captain)
 ManUnited('','','')
