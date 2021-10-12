b0634bf11fe00a060914d630a5132d149f7284d6
Lody G Mtui <lodgmtui@gmail.com>
2021-10-12 16:04:06 +0300

    Update simplefunction.py

--- simplefunction.py
+++ simplefunction.py
@@ -1,6 +1,6 @@
 def ManUnited(players,coach,captain):
     players = ['Pogba','Jesse','Rashford']
-    coach = 'Ole Gunnar Solskjaer'
+    coach = 'Sir Ole Gunnar Solskjaer'
     captain = 'Maguire'
     print(players,coach,captain)
 ManUnited('','','')
